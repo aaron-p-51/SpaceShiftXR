9fe6d4c762de8243f3a97d16dd7d702516908560
Aaron Petrek <petrekaj@gmail.com>
2024-02-26 18:13:32 -0800

    Add SAsteroidPrimaryDataAsset

---
+++ Source/SpaceShiftXR/Public/Gameplay/Asteroid/SAsteroidPrimaryDataAsset.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SAsteroidPrimaryDataAsset.generated.h"
+
+class UStaticMesh;
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESHIFTXR_API USAsteroidPrimaryDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+	/** Static Mesh of asteroid */
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UStaticMesh> Mesh;
+
+	/** Local offset for Mesh component in asteroid actor */
+	UPROPERTY(EditAnywhere)
+	FTransform MeshLocalTransform;
+
+	/** Collision radius of sphere collision component in asteroid actor */
+	UPROPERTY(EditAnywhere)
+	float CollisionRadius;
+
+	/** Minimim world scale of the asteroid actor */
+	UPROPERTY(EditAnywhere)
+	float MinScale;
+
+	/** Maximim world scale of the asteroid actor*/
+	UPROPERTY(EditAnywhere)
+	float MaxScale;
+
+	/** Heath of acteroid, used to set amount of damage it can take before being destroyed*/
+	UPROPERTY(EditAnywhere)
+	float Health;
+
+	/** Number of fragments the asteroid actor will break into */
+	UPROPERTY(EditAnywhere)
+	int32 FragmentCount;
+
+	/** Minimim impulse magnitude of the fragments when breaking away from asteroid */
+	UPROPERTY(EditAnywhere)
+	float MinFragmentImpulseMagnitude;
+
+	/** Maximum impulse magnitude of the fragments when breaking away from asteroid */
+	UPROPERTY(EditAnywhere)
+	float MaxFragmentImpulseMagnitude;
+
+	/** Data assets for fragment asteroids */
+	UPROPERTY(EditAnywhere)
+	TArray<TObjectPtr<USAsteroidPrimaryDataAsset>> AsteroidFragments;
+	
+};
