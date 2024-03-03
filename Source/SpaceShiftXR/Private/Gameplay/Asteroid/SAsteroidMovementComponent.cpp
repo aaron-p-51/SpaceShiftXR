0672e9b0d3587b5e6e5e70c80ca502da9a2e1a6e
Aaron Petrek <petrekaj@gmail.com>
2024-03-03 11:30:25 -0800

    Simple bounce collisions in AsteroidMovementComponent

--- Source/SpaceShiftXR/Private/Gameplay/Asteroid/SAsteroidMovementComponent.cpp
+++ Source/SpaceShiftXR/Private/Gameplay/Asteroid/SAsteroidMovementComponent.cpp
@@ -3,6 +3,16 @@
 
 #include "Gameplay/Asteroid/SAsteroidMovementComponent.h"
 
+
+USAsteroidMovementComponent::USAsteroidMovementComponent()
+{
+	PreviousHitTime = 1.f;
+	bIsSliding = false;
+	PreviousHitNormal = FVector::UpVector;
+	BounceAdditionalIterations = 1;
+	MaxSimulationIterations = 4;
+}
+
 void USAsteroidMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
@@ -19,7 +29,7 @@ void USAsteroidMovementComponent::TickComponent(float DeltaTime, ELevelTick Tick
 	int32 LoopCount = 0;
 	int32 Iterations = 0;
 	FHitResult Hit(1.f);
-	while (bEnableMovement && RemainingTime >= MinTickTime && IsValid(UpdatedComponent))
+	while (bEnableMovement && (RemainingTime >= MinTickTime) && IsValid(UpdatedComponent) && (Iterations < MaxSimulationIterations))
 	{
 
 		LoopCount++;
@@ -34,9 +44,160 @@ void USAsteroidMovementComponent::TickComponent(float DeltaTime, ELevelTick Tick
 		const FVector MoveDelta = ComputeMoveDelta(OldVelocity, TimeTick);
 
 		// Should bounce
-		SafeMoveUpdatedComponent(MoveDelta, UpdatedComponent->GetComponentRotation(), false, Hit);
+		SafeMoveUpdatedComponent(MoveDelta, UpdatedComponent->GetComponentRotation(), true, Hit);
+
+		if (!Hit.bBlockingHit)
+		{
+			PreviousHitTime = 1.f;
+			bIsSliding = false;
+
+			if (Velocity == OldVelocity)
+			{
+				Velocity = ComputeVelocity(Velocity, TimeTick);
+			}
+		}
+		else
+		{
+			// Only calculate new velocity if events didn't change it during the movement update.
+			if (Velocity == OldVelocity)
+			{
+				// re-calculate end velocity for partial time
+				Velocity = (Hit.Time > UE_KINDA_SMALL_NUMBER) ? ComputeVelocity(OldVelocity, TimeTick * Hit.Time) : OldVelocity;
+			}
+
+			NumImpacts++;
+			float SubTickTimeRemaining = TimeTick * (1.f - Hit.Time);
+			const EHandleBlockingHitResult HandleBlockingHitResult = HandleBlockingHit(Hit, TimeTick, MoveDelta, SubTickTimeRemaining);
+			if (HandleBlockingHitResult == EHandleBlockingHitResult::Abort)
+			{
+				break;
+			}
+			else if (HandleBlockingHitResult == EHandleBlockingHitResult::Deflect)
+			{
+				NumBounces++;
+				HandleDeflection(Hit, OldVelocity, NumBounces, SubTickTimeRemaining);
+				PreviousHitTime = Hit.Time;
+				PreviousHitNormal = ConstrainNormalToPlane(Hit.Normal);
+			}
+			else
+			{
+				checkNoEntry();
+			}
+
+			if (SubTickTimeRemaining >= MinTickTime)
+			{
+				RemainingTime += SubTickTimeRemaining;
+
+				if (NumImpacts <= BounceAdditionalIterations)
+				{
+					Iterations--;
+				}
+			}
+
+		}
 
 	}
+
+	if (LoopCount >= 2)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Loop Count: %d"), LoopCount);
+	}
+
+
+	UpdateComponentVelocity();
+}
+
+USAsteroidMovementComponent::EHandleBlockingHitResult USAsteroidMovementComponent::HandleBlockingHit(const FHitResult& Hit, float TimeTick, const FVector& MoveDelta, float& SubTickTimeRemaining)
+{
+	AActor* ActorOwner = UpdatedComponent ? UpdatedComponent->GetOwner() : NULL;
+	if (/*!CheckStillInWorld() ||*/ !IsValid(ActorOwner))
+	{
+		return EHandleBlockingHitResult::Abort;
+	}
+
+	HandleImpact(Hit, TimeTick, MoveDelta);
+
+	if (!IsValid(ActorOwner) /* || HasStoppedSimulation() */ )
+	{
+		return EHandleBlockingHitResult::Abort;
+	}
+
+	if (Hit.bStartPenetrating)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Asteroid %s is stuck inside %s.%s with velocity %s!"), *GetNameSafe(ActorOwner), *Hit.HitObjectHandle.GetName(), *GetNameSafe(Hit.GetComponent()), *Velocity.ToString());
+		return EHandleBlockingHitResult::Abort;
+	}
+
+	SubTickTimeRemaining = TimeTick * (1.f - Hit.Time);
+	return EHandleBlockingHitResult::Deflect;
+}
+
+
+void USAsteroidMovementComponent::HandleImpact(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta)
+{
+	bool bStopSimulating = false;
+
+	if (true /*bShouldBounce*/)
+	{
+		const FVector OldVelocity = Velocity;
+		Velocity = ComputeBounceResult(Hit, TimeSlice, MoveDelta);
+
+		// Trigger bounce events
+		//OnProjectileBounce.Broadcast(Hit, OldVelocity);
+
+		// Event may modify velocity or threshold, so check velocity threshold now.
+		Velocity = LimitVelocity(Velocity);
+		/*if (IsVelocityUnderSimulationThreshold())
+		{
+			bStopSimulating = true;
+		}*/
+	}
+	else
+	{
+		bStopSimulating = true;
+	}
+
+
+	if (bStopSimulating)
+	{
+		StopSimulating(Hit);
+	}
+}
+
+FVector USAsteroidMovementComponent::ComputeBounceResult(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta)
+{
+	FVector TempVelocity = Velocity;
+	const FVector Normal = ConstrainNormalToPlane(Hit.Normal);
+	const float VDotNormal = (TempVelocity | Normal);
+
+	// Only if velocity is opposed by normal or parallel
+	if (VDotNormal <= 0.f)
+	{
+		// Project velocity onto normal in reflected direction.
+		const FVector ProjectedNormal = Normal * -VDotNormal;
+
+		// Point velocity in direction parallel to surface
+		TempVelocity += ProjectedNormal;
+
+		// Only tangential velocity should be affected by friction.
+		//const float ScaledFriction = (bBounceAngleAffectsFriction || bIsSliding) ? FMath::Clamp(-VDotNormal / TempVelocity.Size(), MinFrictionFraction, 1.f) * Friction : Friction;
+		//TempVelocity *= FMath::Clamp(1.f - /*(ScaledFriction*/, 0.f, 1.f);
+
+		// Coefficient of restitution only applies perpendicular to impact.
+		TempVelocity += (ProjectedNormal * FMath::Max(/* Bounciness*/ 1.f, 0.f));
+
+		// Bounciness could cause us to exceed max speed.
+		TempVelocity = LimitVelocity(TempVelocity);
+	}
+
+	return TempVelocity;
+}
+
+void USAsteroidMovementComponent::StopSimulating(const FHitResult& HitResult)
+{
+	Velocity = FVector::ZeroVector;
+	UpdateComponentVelocity();
+	SetUpdatedComponent(NULL);
 }
 
 FVector USAsteroidMovementComponent::ComputeMoveDelta(const FVector& InVelocity, float DeltaTime) const
@@ -88,4 +249,56 @@ FVector USAsteroidMovementComponent::LimitVelocity(FVector NewVelocity) const
 	}
 
 	return ConstrainDirectionToPlane(NewVelocity);
+}
+
+bool USAsteroidMovementComponent::HandleDeflection(FHitResult& Hit, const FVector& OldVelocity, const uint32 NumBounces, float& SubTickTimeRemaining)
+{
+	const FVector Normal = ConstrainNormalToPlane(Hit.Normal);
+
+	// Multiple hits within very short time period?
+	const bool bMultiHit = (PreviousHitTime < 1.f && Hit.Time <= UE_KINDA_SMALL_NUMBER);
+
+	// if velocity still into wall (after HandleBlockingHit() had a chance to adjust), slide along wall
+	const float DotTolerance = 0.01f;
+	/*bIsSliding = (bMultiHit && FVector::Coincident(PreviousHitNormal, Normal)) ||
+		((Velocity.GetSafeNormal() | Normal) <= DotTolerance);*/
+
+	//if (bIsSliding)
+	//{
+	//	if (bMultiHit && (PreviousHitNormal | Normal) <= 0.f)
+	//	{
+	//		//90 degree or less corner, so use cross product for direction
+	//		FVector NewDir = (Normal ^ PreviousHitNormal);
+	//		NewDir = NewDir.GetSafeNormal();
+	//		Velocity = Velocity.ProjectOnToNormal(NewDir);
+	//		if ((OldVelocity | Velocity) < 0.f)
+	//		{
+	//			Velocity *= -1.f;
+	//		}
+	//		Velocity = ConstrainDirectionToPlane(Velocity);
+	//	}
+	//	else
+	//	{
+	//		//adjust to move along new wall
+	//		Velocity = ComputeSlideVector(Velocity, 1.f, Normal, Hit);
+	//	}
+
+	//	// Check min velocity.
+	//	if (IsVelocityUnderSimulationThreshold())
+	//	{
+	//		StopSimulating(Hit);
+	//		return false;
+	//	}
+
+	//	// Velocity is now parallel to the impact surface.
+	//	if (SubTickTimeRemaining > UE_KINDA_SMALL_NUMBER)
+	//	{
+	//		if (!HandleSliding(Hit, SubTickTimeRemaining))
+	//		{
+	//			return false;
+	//		}
+	//	}
+	//}
+
+	return true;
 }
--- Source/SpaceShiftXR/Public/Gameplay/Asteroid/SAsteroidMovementComponent.h
+++ Source/SpaceShiftXR/Public/Gameplay/Asteroid/SAsteroidMovementComponent.h
@@ -16,6 +16,37 @@ class SPACESHIFTXR_API USAsteroidMovementComponent : public UMovementComponent
 
 public:
 
+	USAsteroidMovementComponent();
+
+	// From projectile movement
+	float PreviousHitTime;
+	bool bIsSliding;
+	FVector PreviousHitNormal;
+	/**
+     * On the first few bounces (up to this amount), allow extra iterations over MaxSimulationIterations if necessary.
+     */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0", ClampMax = "4", UIMin = "0", UIMax = "4"), Category = ProjectileSimulation)
+	int32 BounceAdditionalIterations;
+
+	/**
+     * Max number of iterations used for each discrete simulation step.
+     * Increasing this value can address precision issues with fast-moving objects or complex collision scenarios, at the cost of performance.
+     *
+     * WARNING: if (MaxSimulationTimeStep * MaxSimulationIterations) is too low for the min framerate, the last simulation step may exceed MaxSimulationTimeStep to complete the simulation.
+     * @see MaxSimulationTimeStep, bForceSubStepping
+     */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "1", ClampMax = "25", UIMin = "1", UIMax = "25"), Category = ProjectileSimulation)
+	int32 MaxSimulationIterations;
+
+	// Enum indicating how simulation should proceed after HandleBlockingHit() is called.
+	enum class EHandleBlockingHitResult
+	{
+		Deflect,				/** Assume velocity has been deflected, and trigger HandleDeflection(). This is the default return value of HandleBlockingHit(). */
+		AdvanceNextSubstep,		/** Advance to the next simulation update. Typically used when additional slide/multi-bounce logic can be ignored,
+									such as when an object that blocked the projectile is destroyed and movement should continue. */
+		Abort,					/** Abort all further simulation. Typically used when components have been invalidated or simulation should stop. */
+	};
+
 	bool bEnableMovement = false;
 
 
@@ -28,4 +59,9 @@ public:
 	FVector ComputeVelocity(FVector InitialVelocity, float DeltaTime) const;
 	FVector LimitVelocity(FVector NewVelocity) const;
 	FVector ComputeAcceleration(const FVector& InVelocity, float DeltaTime) const;
+	EHandleBlockingHitResult HandleBlockingHit(const FHitResult& Hit, float TimeTick, const FVector& MoveDelta, float& SubTickTimeRemaining);
+	void HandleImpact(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta);
+	void StopSimulating(const FHitResult& HitResult);
+	FVector ComputeBounceResult(const FHitResult& Hit, float TimeSlice, const FVector& MoveDelta);
+	bool HandleDeflection(FHitResult& Hit, const FVector& OldVelocity, const uint32 NumBounces, float& SubTickTimeRemaining);
 };
